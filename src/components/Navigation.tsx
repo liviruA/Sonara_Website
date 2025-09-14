ae178b65f86e0f68b0a60487fcc3f0ceaca47f17
Liviru Abeygunawardena <liviru2003@gmail.com>
2025-09-14 00:51:23 -0400

    Logo Position change

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -9,7 +9,7 @@ const Navigation = () => {
     <nav className="w-full h-20 bg-black shadow-sm sticky top-0 z-50">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 h-20">
         <div className="flex justify-between items-center h-full">
-          <div className="flex-shrink-0 flex items-center">
+          <div className="flex items-center justify-center">
             <img className="h-16 w-auto" src={Logo} alt="Sonara Services logo" />
           </div>
           
